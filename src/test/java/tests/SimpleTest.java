cd28a56f0f667fbabbba5605552af85a72b1cdb5
RDRv4 <75618413+RDRv4@users.noreply.github.com>
2025-12-26 01:37:00 +0300

    prop

--- src/test/java/tests/SimpleTest.java
+++ src/test/java/tests/SimpleTest.java
@@ -1,10 +1,12 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class SimpleTest {
 
     @Test
+    @Tag("simple")
     void simpleTest () {
         String string = System.getProperty("browser", "opera");
         System.out.println(string);
