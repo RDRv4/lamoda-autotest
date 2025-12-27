852b5159bf310d6a0e87d539087c6cb1af06f69d
RDRv4 <75618413+RDRv4@users.noreply.github.com>
2025-12-27 12:30:27 +0300

    main cimmit with fix browser version

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,12 +2,18 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.clearBrowserCookies;
+import static com.codeborne.selenide.Selenide.clearBrowserLocalStorage;
+import static io.qameta.allure.Allure.step;
 
 public class TestBase {
 
@@ -15,8 +21,8 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = System.getProperty("base_url", "https://www.lamoda.by/");
         Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
-        Configuration.browser = System.getProperty("browser", "firefox");
-        Configuration.browserVersion = System.getProperty("browser_version", "124.0");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "128.0");
         Configuration.timeout = 10000;
 
         
@@ -36,16 +42,16 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
-//    @AfterEach
-//    void addAttachments() {
-//        Attach.screenshotAs("Last screenshot");
-//        Attach.pageSource();
-//        Attach.browserConsoleLogs();
-//        Attach.addVideo();
-//        step("Очищаем cookies", () -> {
-//            clearBrowserCookies();
-//            clearBrowserLocalStorage();
-//        });
-//
-//    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        step("Очищаем cookies", () -> {
+            clearBrowserCookies();
+            clearBrowserLocalStorage();
+        });
+
+    }
 }
