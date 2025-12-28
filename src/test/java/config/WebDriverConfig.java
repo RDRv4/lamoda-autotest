fcac50b407dc7d442b81342e563046ef82dcf646
RDRv4 <75618413+RDRv4@users.noreply.github.com>
2025-12-28 17:40:37 +0300

    add local & remote property

---
+++ src/test/java/config/WebDriverConfig.java
@@ -0,0 +1,29 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:${env}.properties",
+        "classpath:local.properties",
+})
+public interface WebDriverConfig extends Config {
+
+    @Key("browser")
+    @DefaultValue("chrome")
+    String browser();
+
+    @Key("browserVersion")
+    @DefaultValue("128.0")
+    String browserVersion();
+
+    @Key("browserResolution")
+    @DefaultValue("1920x1080")
+    String browserResolution();
+
+    @Key("remoteUrl")
+    String remoteUrl();
+
+    @Key("baseUrl")
+    @DefaultValue("https://www.lamoda.by/")
+    String baseUrl();
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,32 +2,29 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import helpers.Attach;
+import config.WebDriverConfig;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
-import static com.codeborne.selenide.Selenide.clearBrowserCookies;
-import static com.codeborne.selenide.Selenide.clearBrowserLocalStorage;
-import static io.qameta.allure.Allure.step;
-
 public class TestBase {
 
+    private static final WebDriverConfig config = ConfigFactory.create(
+            WebDriverConfig.class,
+            System.getProperties()
+    );
+
     @BeforeAll
-    static void beforeAll() {
-        Configuration.baseUrl = System.getProperty("base_url", "https://www.lamoda.by/");
-        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browser_version", "128.0");
-        Configuration.timeout = 10000;
-
-        
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
+    static void setWebDriver() {
+        Configuration.baseUrl = config.baseUrl();
+        Configuration.browser = config.browser();
+        Configuration.browserVersion = config.browserVersion();
+        Configuration.browserSize = config.browserResolution();
+        Configuration.remote = config.remoteUrl();
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
@@ -37,21 +34,21 @@ public class TestBase {
     }
 
     @BeforeEach
-    void addListener() {
-
+    void addListenerAllure() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
-        step("Очищаем cookies", () -> {
-            clearBrowserCookies();
-            clearBrowserLocalStorage();
-        });
 
-    }
+//    @AfterEach
+//    void addAttachments() {
+//        Attach.screenshotAs("Last screenshot");
+//        Attach.pageSource();
+//        Attach.browserConsoleLogs();
+//        Attach.addVideo();
+//        step("Очищаем cookies", () -> {
+//            clearBrowserCookies();
+//            clearBrowserLocalStorage();
+//        });
+//
+//    }
 }
