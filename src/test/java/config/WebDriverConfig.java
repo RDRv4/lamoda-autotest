10531a934ad50d10ac90c61b2b12f8c6d2d87b72
RDRv4 <75618413+RDRv4@users.noreply.github.com>
2025-12-28 17:53:39 +0300

    some changes

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -21,6 +21,7 @@ public interface WebDriverConfig extends Config {
     String browserResolution();
 
     @Key("remoteUrl")
+    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
     String remoteUrl();
 
     @Key("baseUrl")
